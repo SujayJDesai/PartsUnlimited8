40313e53dc595fd4e601ab13c38e716e57481d01
NoReply Amazon <no-reply@amazon.com>
2025-02-11 11:15:50 +0000

    [Commit] Iteration 27 #errors = 250 <= 290.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/IaaS2PaaSWeb/PartsUnlimitedWebsite/Controllers/AccountController.cs][CS0246][13,6] The type or namespace name 'AuthorizeAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- IaaS2PaaSWeb/PartsUnlimitedWebsite/Controllers/AccountController.cs
+++ IaaS2PaaSWeb/PartsUnlimitedWebsite/Controllers/AccountController.cs
@@ -4,9 +4,8 @@ using System.Threading.Tasks;
 using System.Web;
 using PartsUnlimited.Models;
 using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace PartsUnlimited.Controllers
 {
