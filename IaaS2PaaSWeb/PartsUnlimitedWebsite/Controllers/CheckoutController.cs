97700f6339f651afb61ac66f48ca8a845b6eed14
NoReply Amazon <no-reply@amazon.com>
2025-02-11 11:17:48 +0000

    [Commit] Iteration 31 #errors = 234 <= 236.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/IaaS2PaaSWeb/PartsUnlimitedWebsite/Controllers/CheckoutController.cs][CS0246][14,6] The type or namespace name 'AuthorizeAttribute' could not be found (are you missing a using directive or an assembly reference?)``.

--- IaaS2PaaSWeb/PartsUnlimitedWebsite/Controllers/CheckoutController.cs
+++ IaaS2PaaSWeb/PartsUnlimitedWebsite/Controllers/CheckoutController.cs
@@ -5,7 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using PartsUnlimited.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 
