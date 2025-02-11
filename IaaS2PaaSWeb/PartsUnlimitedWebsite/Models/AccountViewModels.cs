a310bbc6e17ddbbf4bfaa1b311f4047bc1691cac
NoReply Amazon <no-reply@amazon.com>
2025-02-11 11:18:38 +0000

    [Commit] Iteration 34 #errors = 230 <= 231.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/IaaS2PaaSWeb/PartsUnlimitedWebsite/Models/AccountViewModels.cs][CS0246][28,28] The type or namespace name 'SelectListItem' could not be found (are you missing a using directive or an assembly reference?)``.

--- IaaS2PaaSWeb/PartsUnlimitedWebsite/Models/AccountViewModels.cs
+++ IaaS2PaaSWeb/PartsUnlimitedWebsite/Models/AccountViewModels.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace PartsUnlimited.Models
