fd2b8b78995b18b1388138d1cabfb8eb34dc073f
NoReply Amazon <no-reply@amazon.com>
2025-02-11 11:09:58 +0000

    [Commit] Iteration 19 #errors = 300 <= 301.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/IaaS2PaaSWeb/PartsUnlimitedWebsite/Migrations/PartsUnlimitedContextModelSnapshot.cs][CS0234][7,22] The type or namespace name 'Data' does not exist in the namespace 'PartsUnlimited' (are you missing an assembly reference?)``.

--- IaaS2PaaSWeb/PartsUnlimitedWebsite/Migrations/PartsUnlimitedContextModelSnapshot.cs
+++ IaaS2PaaSWeb/PartsUnlimitedWebsite/Migrations/PartsUnlimitedContextModelSnapshot.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using PartsUnlimited.Models;
 using System;
-using PartsUnlimited.Data;
 
 namespace PartsUnlimitedWebsite.Migrations
 {
